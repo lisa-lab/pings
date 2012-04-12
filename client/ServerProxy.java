7fee4304f7cddcb25837fe7e83ed3826ff75627c
Christian Hudon <chrish@pianocktail.org>
2012-04-12 11:44:45 -0400

    Close OutputStream properly; use hostname for Pings server instead of base URL.

--- client/ServerProxy.java
+++ client/ServerProxy.java
@@ -1,5 +1,6 @@
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.InetAddress;
 import java.io.*;
 
 import org.json.simple.JSONValue;
@@ -7,40 +8,40 @@ import org.json.simple.JSONValue;
 /** Interface to the Pings server.
  *  @todo Add methods to get addresses to ping, and to submit ping results.
  *  @todo Error handling when the http status returned is not 200.
+ *
+ *  @author Christian Hudon <chrish@pianocktail.org>
  */
 public class ServerProxy {
 
-    public ServerProxy(String server_url) {
-        m_server_url = server_url;
+    public ServerProxy(String server_hostname) {
+        m_server_hostname = server_hostname;
     }
 
-    /** The base URL for the Pings server. Paths for specific RPC methods
-        are added to this. */
-    private String m_server_url;
+    /// The hostname for the Pings server.
+    private String m_server_hostname;
     private static final String CHARSET = "UTF-8";
 
-    /** Sends a JSON object via POST to the given URL path. Returns the
+    /** Sends a JSON object via POST to the given request URL path. Returns the
         JSON response object. */
-    private Object doJsonRequest(String url_path, Object content) throws IOException {
+    private Object doJsonRequest(String request_path, Object content) throws IOException {
         // Serialize content to JSON.
         String json_request = JSONValue.toJSONString(content);
 
         // Open connection to URL.
-        URL full_url = new URL(m_server_url + url_path);
+        URL server_url = new URL("http", m_server_hostname, request_path);
 
-        HttpURLConnection connection = (HttpURLConnection)full_url.openConnection();
+        HttpURLConnection connection = (HttpURLConnection)server_url.openConnection();
         connection.setRequestMethod("POST");
         connection.setRequestProperty("Accept-Charset", CHARSET);
         connection.setRequestProperty("Content-Type", "application/json;charset=" + CHARSET);
 
         // Write request.
-        OutputStream output = null;
+        OutputStream output = connection.getOutputStream();
         try {
-            connection.getOutputStream().write(json_request.getBytes(CHARSET));
+            output.write(json_request.getBytes(CHARSET));
         }
         finally {
-            if (output != null)
-                output.close();
+            output.close();
         }
 
         int status = connection.getResponseCode();

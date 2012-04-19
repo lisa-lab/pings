aa4d5da6665d6582a0730f529426d34f080b6322
Christian Hudon <chrish@pianocktail.org>
2012-04-19 17:49:17 -0400

    Add code to create Upstart configurations for and start the Pings servers.

--- server/fabfile.py
+++ server/fabfile.py
@@ -2,6 +2,7 @@
 Web Services. Tested with Fabric 1.4.1."""
 
 import os.path
+from StringIO import StringIO
 from fabric.api import *
 from fabric.contrib.files import exists
 
@@ -11,6 +12,9 @@ from fabconfig import *
 from setup import version as pings_version
 here = os.path.dirname(__file__)
 
+# TODO Create (in prepare_host) and use a different user for all the pings
+# servers, as the 'ubuntu' user can sudo to root without a password.
+PINGS_USER = 'ubuntu'
 
 @task
 def prepare_source():
@@ -58,17 +62,20 @@ def bootstrap_python_install():
 @task
 def prepare_memcache_role():
     """Setup a computer for the memcache server role."""
+    # A reinstall should not restart the Memcache server needlessly, otherwise
+    # we will lose the tokens in production every time this is run.
     install_system_packages(['memcached'])
 
 @task
 def prepare_leaderboard_role():
     """Setup a computer for the leaderboard server role."""
+    # A reinstall must not restart the Redis server needlessly, otherwise
+    # we will lose the leaderboards in production every time this is run.
     install_system_packages(['redis-server'])
 
 @task
 def prepare_host():
     """Install all system base packages, basic Python environment, etc."""
-    # TODO Remove ability to sudo without a password on remote host!
     install_system_base_packages()
     bootstrap_python_install()
 
@@ -81,17 +88,78 @@ def prepare_test_host():
     prepare_memcache_role()
     prepare_leaderboard_role()
 
+def generate_upstart_conf(rootdir, program, args, description):
+    """Create a .conf file for an Upstart service. Returns it
+    as a StringIO object."""
+    name = os.path.basename(program)
+    return StringIO('''description "%(description)s"
+
+start on runlevel [2345]
+stop on runlevel [!2345]
+respawn
+
+# Supported starting with Ubuntu 12.04
+#console log
+#setuid ubuntu
+
+# We need to specify an absolute path for the program even though
+# we use --chdir to work around the following bug:
+# http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=669047
+exec start-stop-daemon --start --chdir %(rootdir)s --chuid ubuntu --name %(name)s --startas %(rootdir)s/%(program)s -- %(args)s
+''' % locals())
+
+def start_upstart_service(name, rootdir, program, args, description):
+    """Create an Upstart service .conf file, uploads it and starts the service."""
+    put(generate_upstart_conf(rootdir, program, args, description),
+        '/etc/init/%s.conf' % name, use_sudo=True)
+    # Need to do a stop + start for Upstart to use the new config.
+    with settings(warn_only=True):
+        sudo('stop %s' % name)
+    sudo('start %s' % name)
+
+@task
+def start_http_server(rootdir):
+    """Starts the Pings http server."""
+    start_upstart_service('pings-http-server', rootdir, program='bin/paster',
+                          args='serve development.ini',
+                          description='Pings http server')
+
+@task
+def start_storage_server(rootdir):
+    """Starts the Pings storage server."""
+    data_dir = '%s/data' % rootdir
+    sudo('mkdir -p ' + data_dir)
+    sudo('chown %s. %s' % (PINGS_USER, data_dir))
+    start_upstart_service('pings-storage-server', rootdir,
+                          program='bin/storage_server',
+                          args='development.ini %s' % data_dir,
+                          description='Pings storage server')
+
+@task
+def start_leaderboards_server(rootdir, wipe_data=False):
+    """Starts the Pings leaderboards server."""
+    if not wipe_data:
+        # TODO Save data before stopping the service.
+        raise NotImplementedError
+    start_upstart_service('pings-leaderboards-server', rootdir,
+                          program='bin/leaderboards_server', args='development.ini',
+                          description='Pings leaderboards server')
+
 @task
 @roles('test')
 def deploy_test():
-    """Installs everything on a single test server, able to run the development.ini file configuration.
+    """Installs everything on a single test server, able to run
+    the development.ini file configuration, then starts all
+    the Pings servers.
+
     Not to be used for deployment!"""
     prepare_source()
     pings_src_dir = upload_source()
 
     # Install everything
-    sudo('virtualenv --distribute /srv/pings_test')
-    with cd('/srv/pings_test'):
+    rootdir = '/srv/pings_test'
+    sudo('virtualenv --distribute ' + rootdir)
+    with cd(rootdir):
         put('development.ini', '.', use_sudo=True)
 
         # TODO Remove hardcoded python version in path.
@@ -109,3 +177,8 @@ def deploy_test():
         # there so the version we have now is installed even if the version
         # number wasn't bumped up (kinda handy during development).
         sudo('bin/pip install --no-deps --force-reinstall %s' % pings_src_dir)
+
+    # Install and start all the services
+    start_leaderboards_server(rootdir, wipe_data=True)
+    start_storage_server(rootdir)
+    start_http_server(rootdir)

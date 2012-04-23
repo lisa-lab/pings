62f1af60ae1a2b56656628e7f54f4b640ef2163c
Christian Hudon <chrish@pianocktail.org>
2012-04-23 15:13:12 -0400

    Add skeleton for Pings applet.

---
+++ client/PingsApplet.java
@@ -0,0 +1,14 @@
+import java.applet.*;
+import java.awt.*;
+
+public class PingsApplet extends Applet {
+
+    public void init() {
+        // Initialization (on page load) here.
+    }
+
+    public void paint(Graphics g) {
+        // Draw applet here.
+        g.setColor(Color.gray);
+    }
+}

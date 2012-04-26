edd4c460c3fbadb10d1004ee5c03d9505584cfbc
Christian Hudon <chrish@pianocktail.org>
2012-04-26 12:01:14 -0400

    Install a fix for busy-looping problem. Improve documentation of concurrent access restrictions for instance variables.

--- client/PingsClient.java
+++ client/PingsClient.java
@@ -6,13 +6,23 @@ import java.util.concurrent.atomic.AtomicReference;
 /** Pings client. Connects to the Pings server, retrieves addresses
  *  to be pinged, pings them and submits the results back to the server.
  *
+ * @todo IMPORTANT: Give up after a certain number of errors in a row,
+ * instead of continuously hammering the server! If doing more than a few
+ * retries, exponential backoff would probably be a good idea too.
+ * @todo Improve error handling when server_proxy raises an exception.
  * @author Christian Hudon <chrish@pianocktail.org>
  */
 public class PingsClient extends Thread {
+    // These variables are initialized in the constructor and then
+    // only accessed by the PingsClient thread. No need for locking, etc.
     private ClientInfo m_client_info;
     private IcmpPinger m_pinger;
     private ServerProxy m_server_proxy;
 
+    // These variables are accessed both by the PingsClient thread and
+    // by other threads using this class. They need to use something
+    // like the Java Atomic types or other to prevent fun multithreading
+    // bugs!
     private AtomicBoolean m_is_running;
     private AtomicReference<String> m_nick;
 
@@ -32,7 +42,6 @@ public class PingsClient extends Thread {
         m_is_running.set(false);
     }
 
-    /// @todo Improve error handling when server_proxy raises an exception.
     public void run() {
         while (m_is_running.get()) {
             try {
@@ -49,8 +58,13 @@ public class PingsClient extends Thread {
                 m_server_proxy.submitResults(m_client_info, pings);
             }
             catch (IOException e) {
-                // XXX Particularly bad. Thread will busy-loop if IOException keeps
-                // getting raised in call to getPings.
+                // Avoid thread busy-loop if IOException keeps getting raised
+                // in call to getPings.
+                try {
+                    sleep(1000);
+                }
+                catch (InterruptedException e2) {
+                }
             }
         }
     }

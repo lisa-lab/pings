5f47498ae1bde5b693e38f0ae9ee50641920d057
RaphaelBonaque <bonaque@crans.org>
2012-05-04 13:36:07 -0400

    Add an alternative constructor for GeoipInfo (for testing purpose only)

--- client/GeoipInfo.java
+++ client/GeoipInfo.java
@@ -1,6 +1,6 @@
 import org.json.simple.JSONObject;
 
-/** Represetation of the geoip information for a given IP address, created
+/** Representation of the geoip information for a given IP address, created
     from a JSON representation of the information returned by the Pings server.
     Includes the longitude and latitude, and the city and country.
  */
@@ -17,9 +17,22 @@ public class GeoipInfo {
     public GeoipInfo(JSONObject json) {
         city = (String)json.get("city");
         country = (String)json.get("country_name");
-
+        
+        /*FIXME : jsonobject get cast into doubles which are cast into floats 
+        and then cast back into double*/
         longitude = ((Double)json.get("longitude")).floatValue();
         latitude = ((Double)json.get("latitude")).floatValue();
+    }
+    
+    /*
+     * An alternative constructor for testing purpose
+     */
+    //TODO : remove it
+    public GeoipInfo(String city,String country,double longitude,double latitude) {
+    	this.city = city;
+    	this.country = country;
+    	this.longitude = longitude;
+    	this.latitude = latitude;
     }
 }
 

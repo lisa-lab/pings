9d5e735d75d9a0e06a3c33ab00d5de47425e8f19
Raphaelbonaque <bonaque@crans.org>
2012-06-06 11:28:29 -0400

    Reworking the multithreading

--- client/PingsApplet.java
+++ client/PingsApplet.java
@@ -8,16 +8,19 @@ import javax.swing.JApplet;
 */
 public class PingsApplet extends JApplet {
 	
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 2L;
 	
-	private final String SERVER_HOSTNAME = "ec2-184-72-202-57.compute-1.amazonaws.com";
-	private final int SERVER_PORT = 6543;
-	private final String initial_nickname = "no name";
-	public final int nb_client_threads = 5;
-	public Thread[] pings_clients_threads;
+	private final String SERVER_HOSTNAME = "pings-prod-lb-838066006.us-east-1.elb.amazonaws.com";
+	private final int SERVER_PORT = 80;
+	private final String initial_nickname = "No Name";
+	public final int nb_clients = 1;
 	public PingsClient[] pings_clients;
 	
+	private PingsGUI ping_gui;
+	
 	private boolean simulation;
+
+	
 	
 	/**
 	* The initialization of the applet, it creates the PingsClient and the GUI
@@ -26,9 +29,7 @@ public class PingsApplet extends JApplet {
 	* @see  #initGUI()s
 	*/
 	public void init() {
-		
-		pings_clients_threads = new Thread[nb_client_threads];
-		pings_clients = new PingsClient[nb_client_threads];
+		pings_clients = new PingsClient[nb_clients];
 		
 		try {
 			simulation = Boolean.parseBoolean(getParameter("simulation"));
@@ -37,7 +38,7 @@ public class PingsApplet extends JApplet {
 			simulation = false;
 		}
 		
-		for (int i = 0; i < nb_client_threads; i++) {
+		for (int i = 0; i < nb_clients; i++) {
 			if (simulation) {
 				pings_clients[i] = (PingsClient) new PingsClientSimulation();
 			}
@@ -45,33 +46,42 @@ public class PingsApplet extends JApplet {
 				pings_clients[i] = new PingsClient(SERVER_HOSTNAME, SERVER_PORT);
 			}
 			pings_clients[i].setNickname(initial_nickname);
-			pings_clients_threads[i] = new Thread(pings_clients[i]);
 		}
 		
-		new PingsGUI(this);
+		ping_gui = new PingsGUI(this);
 		
-		for (int i = 0; i < nb_client_threads; i++) {
-			pings_clients_threads[i].start();
+		for (int i = 0; i < nb_clients; i++) {
+			pings_clients[i].run();
 			}
 		
 	}
 	
 	public void start () {
-		for (int i = 0; i < nb_client_threads; i++) {
+		for (int i = 0; i < nb_clients; i++) {
 			pings_clients[i].resume();
 			}
 		this.repaint();
 	}
 	
+	
 	public void stop () {
-		for (int i = 0; i < nb_client_threads; i++) {
+		for (int i = 0; i < nb_clients; i++) {
 			pings_clients[i].pause();
 			}
 	}
 	
 	public void destroy() {
-		for (int i = 0; i < nb_client_threads; i++) {
-			pings_clients_threads[i].interrupt();
+		for (int i = 0; i < nb_clients; i++) {
+			pings_clients[i].destroy();
 			}
 		}
+
+	public void errorConnectingServer() {
+		ping_gui.destroy();
+		
+	}
+	
+	public void restartApplet() {
+		
+	}
 }

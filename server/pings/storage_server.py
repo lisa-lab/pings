12b323f2e259cb18da972725f142e556d455bc85
Frederic <nouiz@nouiz.org>
2012-12-12 11:39:31 -0500

    Add host name to the storage file to help prevent corruption.

--- server/pings/storage_server.py
+++ server/pings/storage_server.py
@@ -64,8 +64,9 @@ class OnDemandFile:
             force_new_file = True
 
         if force_new_file or time_period != self.current_time_period:
-            filename = '%02dh%02d-UTC.data' % (dt.hour, int(dt.minute / 10) *
-                                               10)
+            filename = '%02dh%02d-UTC.%s.data' % (dt.hour,
+                                                  int(dt.minute / 10) * 10,
+                                                  socket.gethostname())
 
             if self.f is not None:
                 self.f.close()

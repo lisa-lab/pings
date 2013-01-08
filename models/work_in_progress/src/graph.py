b52f8cce46ce1963214fe4e13cc6e587f803b87a
Nicolas Boulanger-Lewandowski <nicolas_boulanger@hotmail.com>
2013-01-07 22:07:02 -0500

    web server, json, and incorporated html version of analysis

--- models/work_in_progress/src/graph.py
+++ models/work_in_progress/src/graph.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 
 import test_data
 import utils
@@ -11,6 +12,10 @@ from collections import defaultdict
 import scipy.optimize
 import pylab
 from scipy.special import erf
+from wsgiref.simple_server import make_server
+from cgi import parse_qs
+import json
+
 
 
 # load data
@@ -57,92 +62,264 @@ for i in xrange(3):
       last_index = index
 
 
-# fake input
-ip = '203.201.132.74'  # '70.82.10.129'
-one = lambda : '.'.join(map(str, numpy.random.randint(256, size=4))) + ',' + str(max(51.0, numpy.random.normal(loc=400.0, scale=200.0)))
-measurements = ';'.join([one() for i in xrange(20)])
-
-
-# compute stats for request
-ip_int = utils.get_int_from_ip(ip)
-d = utils.get_geoip_data(ip)
-t = d['country_code'], d['region_name'], d['city']
-r = [bisect.bisect_left(names[i], t[:i+1] if i else t[0]) for i in xrange(3)]
-
-all_measurements = measurements.split(';')
-formatted = numpy.empty((len(all_measurements), 15), dtype=int)
-for j, m in enumerate(all_measurements):
-  ip2, latency = m.split(',')
-  latency = float(latency)
-  ip2_int = utils.get_int_from_ip(ip2)
-  d2 = utils.get_geoip_data(ip2)
-  if d2 is not None:
-    t2 = d2['country_code'], d2['region_name'], d2['city']
-    r2 = [bisect.bisect_left(names[i], t2[:i+1] if i else t2[0]) for i in xrange(3)]
-    distance = utils.geoip_distance(d, d2)
-  else:
-    t2 = '', '', ''
-    r2 = -1, -1, -1
-    distance = -1
-  formatted[j, :] = r[0], r[1], r[2], -1, r2[0], r2[1], r2[2], -1, distance, latency, r[0]==r2[0], r[1]==r2[1], r[2]==r2[2], ip_int, ip2_int
-
-
-predictions = numpy.zeros(len(formatted))
-for (feature_indices, _), (p, n, _) in zip(terms, params):
-  feature_indices = list(feature_indices)
-  strides = numpy.array([numpy.prod(sizes[feature_indices[:i]]) for i in xrange(len(feature_indices) + 1)], dtype='uint64')
-  features = formatted[:, feature_indices]
-  known = (features != -1).all(axis=1)
-  features = numpy.sum(features.astype('uint32') * strides[:-1], axis=1)
-  regression = 8 in feature_indices  # hardcoded
-  
-  if regression:
-    predictions[known] += p * features[known].astype(float) + n
-  elif isinstance(p, numpy.ndarray):
-    predictions[known] += p[features[known]]
-  else:
-    for j, f in enumerate(features):
-      if known[j] and p.has_key(f):
-        predictions[j] -= p[f]
-
-
-# online learning (adjust predictions)
-targets = formatted[:, TARGET]
-error = 0.0
-for i, (t, p) in enumerate(zip(targets, predictions)):
-  if i > 0:
-    predictions[i] += error / (i + 5.0)  # ad-hoc regularization
-  error += t - p
-
-
-# show stats for request
+# utility functions
 def rank(i, n):
   return 50 * (n-i+1) / n
-print d['country_name'], stats[0][r[0]], order[0][0][r[0]], rank(*order[0][0][r[0]])
-print d['region_name'], stats[1][r[1]], order[1][0][r[1]], order[1][1][r[1]], rank(*order[1][0][r[1]])
-print d['city'], stats[2][r[2]], order[2][0][r[2]], order[2][1][r[2]], order[2][2][r[2]], rank(*order[2][0][r[2]])
+
+suffixes = 'st', 'nd', 'rd', 'th'
+def pretty_rank(i):
+  return '%i<sup>%s</sup>' % (i, suffixes[min(len(suffixes)-1, (i + 9) % 10)])
 
 def percentile(value, mean, std):
   best_than = -erf((value-mean) / std)*0.5 + 0.5
   return best_than, int(50*best_than)
-you_mean = targets.mean()
-print 'you', percentile(you_mean, *world_stats), '*', percentile(you_mean, *stats[0][r[0]]), percentile(you_mean, *stats[1][r[1]]), percentile(you_mean, *stats[2][r[2]])
 
-print 'average latency (predicted)', predictions.mean()
-print 'average latency (measured)', you_mean
-print 'accuracy of predictions', abs(targets-predictions).mean()
+def sprint(*args):
+  return ' '.join(map(unicode, args)) + '\n'
 
-# shuffle for display
-indices = (formatted[:, 6] != -1).nonzero()[0]  # must have resolved city name to be displayed
-numpy.random.shuffle(indices)
-targets = targets[indices]
-formatted = formatted[indices]
-predictions = predictions[indices]
-
-for i in xrange(min(5, len(targets))):
-  print names[2][formatted[i, 6]][2], names[0][formatted[i, 4]], int(numpy.round(predictions[i])), '(pred)', targets[i], '(measured)'
+def stars_html(score):  # score from 0 to 50
+  code = ''
+  for i in xrange(5):
+    c = (score >= i*10 + 5) + (score >= i*10 + 10)  # 2: full, 1: half, 0: empty
+    code += '<img src="http://iconnect.iro.umontreal.ca/images/star%i.png">' % c
+  return code
 
 
+def display_stats(ip, measurements, html=True, full_page=True):
+  # fake input for testing
+  if ip is None:
+    ip = '203.201.132.74'  # '70.82.10.129'
+  if measurements == '':
+    one = lambda : '.'.join(map(str, numpy.random.randint(256, size=4))) + ',' + str(max(51.0, numpy.random.normal(loc=400.0, scale=200.0)))
+    measurements = '-'.join([one() for i in xrange(20)])
+
+
+  # compute stats for request
+  ip_int = utils.get_int_from_ip(ip)
+  d = utils.get_geoip_data(ip)
+  t = d['country_code'], d['region_name'], d['city']
+  r = [bisect.bisect_left(names[i], t[:i+1] if i else t[0]) for i in xrange(3)]
+
+  all_measurements = measurements.split('-')
+  formatted = numpy.empty((len(all_measurements), 15), dtype=int)
+  saved_geo_data = []
+  for j, m in enumerate(all_measurements):
+    ip2, latency = m.split(',')
+    latency = float(latency)
+    ip2_int = utils.get_int_from_ip(ip2)
+    d2 = utils.get_geoip_data(ip2)
+    saved_geo_data.append(d2)
+    if d2 is not None:
+      t2 = d2['country_code'], d2['region_name'], d2['city']
+      r2 = [bisect.bisect_left(names[i], t2[:i+1] if i else t2[0]) for i in xrange(3)]
+      distance = utils.geoip_distance(d, d2)
+    else:
+      t2 = '', '', ''
+      r2 = -1, -1, -1
+      distance = -1
+    formatted[j, :] = r[0], r[1], r[2], -1, r2[0], r2[1], r2[2], -1, distance, latency, r[0]==r2[0], r[1]==r2[1], r[2]==r2[2], ip_int, ip2_int
+
+
+  predictions = numpy.zeros(len(formatted))
+  for (feature_indices, _), (p, n, _) in zip(terms, params):
+    feature_indices = list(feature_indices)
+    strides = numpy.array([numpy.prod(sizes[feature_indices[:i]]) for i in xrange(len(feature_indices) + 1)], dtype='uint64')
+    features = formatted[:, feature_indices]
+    known = (features != -1).all(axis=1)
+    features = numpy.sum(features.astype('uint32') * strides[:-1], axis=1)
+    regression = 8 in feature_indices  # hardcoded
+    
+    if regression:
+      predictions[known] += p * features[known].astype(float) + n
+    elif isinstance(p, numpy.ndarray):
+      predictions[known] += p[features[known]]
+    else:
+      for j, f in enumerate(features):
+        if known[j] and p.has_key(f):
+          predictions[j] -= p[f]
+
+
+  # online learning (adjust predictions)
+  targets = formatted[:, TARGET]
+  error = 0.0
+  for i, (t, p) in enumerate(zip(targets, predictions)):
+    if i > 0:
+      predictions[i] += error / (i + 5.0)  # ad-hoc regularization
+    error += t - p
+
+
+  # shuffle for display
+  indices = (formatted[:, 6] != -1).nonzero()[0]  # must have resolved city name to be displayed
+  numpy.random.shuffle(indices)
+  indices = indices[:5]
+
+
+  # show stats for request (text)
+  you_mean = targets.mean()
+  if not html:
+    response = u''
+    response += sprint(d['country_name'], stats[0][r[0]], order[0][0][r[0]], rank(*order[0][0][r[0]]))
+    response += sprint(d['region_name'], stats[1][r[1]], order[1][0][r[1]], order[1][1][r[1]], rank(*order[1][0][r[1]]))
+    response += sprint(d['city'], stats[2][r[2]], order[2][0][r[2]], order[2][1][r[2]], order[2][2][r[2]], rank(*order[2][0][r[2]]))
+    response += sprint('you', percentile(you_mean, *world_stats), '*', percentile(you_mean, *stats[0][r[0]]), percentile(you_mean, *stats[1][r[1]]), percentile(you_mean, *stats[2][r[2]]))
+
+    response += sprint('average latency (predicted)', predictions.mean())
+    response += sprint('average latency (measured)', you_mean)
+    response += sprint('accuracy of predictions', abs(targets-predictions).mean())
+    for i in indices:
+      response += sprint(names[2][formatted[i, 6]][2], names[0][formatted[i, 4]], int(numpy.round(predictions[i])), '(pred)', targets[i], '(measured)')
+    return response
+
+  # show stats (html)
+  region_term = {'CA': 'province', 'US': 'state'}.get(d['country_code'], 'region')
+  
+  response = u''
+
+  if full_page: response += u"""<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+ "http://www.w3.org/TR/html4/loose.dtd">
+
+<html lang="en">
+
+ <head>
+  <title>Analysis Results - The Internet Connectome project</title>
+  <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+  <link href="http://iconnect.iro.umontreal.ca/site.css" media="all" type="text/css" rel="stylesheet">
+  <script type="text/javascript" src="http://code.jquery.com/jquery-1.8.3.min.js"></script>
+
+<script>
+function show(index, self) {
+  tooltip = $('#tooltip' + index);
+  tooltip.show();
+  position = $(self).position();
+  position.top += 42;
+  position.left += 14;
+  tooltip.offset(position);
+}
+function hide(index) {
+  $('#tooltip' + index).hide();
+}
+</script>
+
+ </head>
+
+ <body>
+
+<center>
+"""
+
+  response += u"""
+<div class="main_div analysis">
+
+<div class="title">Analysis Results</div>
+
+<div class="subtitle">Thank you for your participation !</div>
+
+<h2>Your network location</h2>
+
+<center><table cellspacing="0" cellpadding="0">
+
+<tr><th style="text-align: left;">location</th>
+<th>average<br />latency (ms) &dagger;</th>
+<th></th></tr>
+
+<tr onmouseover="show(1,this)" onmouseout="hide(1)" ><td class="left_column">""" + d['country_name'] + u"""</td>
+<td class="middle_column">""" + str(int(numpy.round(float(stats[0][r[0]][0])))) + u"""</td>
+<td style="min-width: 200px;">""" + stars_html(rank(*order[0][0][r[0]])) + u"""</td></tr>
+
+<tr onmouseover="show(2,this)" onmouseout="hide(2)" ><td class="left_column">""" + d['region_name'] + u"""</td>
+<td class="middle_column">""" + str(int(numpy.round(float(stats[1][r[1]][0])))) + u"""</td>
+<td>""" + stars_html(rank(*order[1][0][r[1]])) + u"""</td></tr>
+
+<tr onmouseover="show(3,this)" onmouseout="hide(3)" ><td class="left_column">""" + d['city'] + u"""</td>
+<td class="middle_column">""" + str(int(numpy.round(float(stats[2][r[2]][0])))) + u"""</td>
+<td>""" + stars_html(rank(*order[2][0][r[2]])) + u"""</td></tr>
+
+<tr onmouseover="show(4,this)" onmouseout="hide(4)" style="background: #fec;"><td class="left_column">Overall</td>
+<td class="middle_column">""" + str(int(numpy.round(you_mean))) + u"""</td>
+<td>""" + stars_html(percentile(you_mean, *world_stats)[1]) + u"""</td></tr>
+
+</table></center>
+
+<p style="font-size: 0.9em;">&dagger; World average is estimated at """ + str(int(numpy.round(world_stats[0]))) + u""" ms.</p>
+
+<div id="tooltip1" class="tooltip"><u>%s</u> is:<br />Ranked %s of %i countries.</div>""" % (d['country_name'], pretty_rank(order[0][0][r[0]][0]), order[0][0][r[0]][1]) + u"""
+<div id="tooltip2" class="tooltip"><u>%s</u> is:<br />Ranked %s %s in %s.</div>""" % (d['region_name'], pretty_rank(order[1][1][r[1]][0]), region_term, d['country_name']) + u"""
+<div id="tooltip3" class="tooltip"><u>%s</u> is:<br />Ranked %s of %i cities in %s.<br />Ranked %s of %i cities in %s.<br />Ranked %s of %i cities in our database.</div>""" % (d['city'], pretty_rank(order[2][2][r[2]][0]), order[2][2][r[2]][1], d['region_name'], pretty_rank(order[2][1][r[2]][0]), order[2][1][r[2]][1], d['country_name'], pretty_rank(order[2][0][r[2]][0]), order[2][0][r[2]][1]) + u"""
+<div id="tooltip4" class="tooltip"><u>YOU</u> have an average latency of %i ms which is:<br />Faster than %i%% of the world.<br />Faster than %i%% of people in %s.<br />Faster than %i%% of people in %s.<br />Faster than %i%% of people in %s.</div>""" % (int(numpy.round(you_mean)), int(100*percentile(you_mean, *world_stats)[0]), int(100*percentile(you_mean, *stats[0][r[0]])[0]), d['country_name'], int(100*percentile(you_mean, *stats[1][r[1]])[0]), d['region_name'], int(100*percentile(you_mean, *stats[2][r[2]])[0]), d['city']) + u"""
+
+
+<h2>Latency measurements</h2>
+
+<center><table cellspacing="0" cellpadding="0">
+<tr><td>Average latency (predicted)</td><td><b>%i ms</b></td></tr>
+<tr><td>Average latency (measured)</td><td><b>%i ms</b></td></tr>
+<tr><td style="width: 205px;">Accuracy of prediction</td><td><b>%i ms</b></td></tr>""" % (int(numpy.round(predictions.mean())), int(numpy.round(you_mean)), int(numpy.round(abs(targets-predictions).mean())))
+
+  max_width = 220
+  max_seen = max([max(int(numpy.round(predictions[i])), indices[i]) for i in indices])
+  for i in indices:
+    width_prediction = int(max_width * numpy.round(predictions[i]) / max_seen)
+    width_target = int(max_width * float(targets[i]) / max_seen)
+    response += u"""  
+<tr><td>%s, %s</td><td>
+<div style="width: %ipx;" class="bar top"></div><div class="label">%i ms</div><div style="clear: both;"></div>
+<div style="width: %ipx;" class="bar"></div><div class="label bottom">%i ms</div><div style="clear: both;"></div>
+</td></tr>""" % (saved_geo_data[i]['city'], saved_geo_data[i]['country_name'], width_prediction, int(numpy.round(predictions[i])), width_target, targets[i])
+
+  response += u"""
+<tr style="background: #fec;"><td colspan="2">
+<div style="float: left; font-weight: bold;">Legend:</div>
+<div style="width: 25px; height: 5px; background: #6D7D57; float: left; margin: 6px 5px 6px 40px;"></div><div style="float: left; font-size: 0.85em; color: #888;">Prediction</div>
+<div style="width: 25px; height: 5px; background: #E35A00; float: left; margin: 6px 5px 6px 25px;"></div><div style="float: left; font-size: 0.85em; color: #888;">Measurement</div>
+<div style="clear: both;"></div>
+</td></tr>
+
+</table></center>
+
+</div>
+"""
+
+  if full_page: response += u"""
+</center>
+
+</body>
+ 
+</html>
+"""
+  return response
+
+
+# web server
+def application(environment, start_response):
+  if environment['PATH_INFO'] != '/feedback.py':
+    start_response('404 Not Found', [])
+    return []
+  
+  ip = environment['REMOTE_ADDR']
+  parameters = parse_qs(environment['QUERY_STRING'])
+  measurements = parameters.get('measurements', [''])[0]
+  html = not bool(parameters.get('text_only', [''])[0])
+  full_page = bool(parameters.get('full_page', [''])[0])
+  response = display_stats(ip, measurements, html, full_page)
+  if full_page:
+    response = response.encode('utf-8')
+    content_type = 'text/html' if html else 'text/plain'
+  else:
+    response = json.dumps({'content': response})  # already utf-8 encoded
+    content_type = 'application/json'
+  headers = [('Content-Type', content_type), ('Content-Length', str(len(response))), ('charset', 'utf-8')]
+  start_response('200 OK', headers)
+  return [response]
+
+
+if __name__ == '__main__':
+  server = make_server('', 6660, application)
+  print 'Server running ...'
+  sys.stdout.flush()
+  try:
+    server.serve_forever()
+  except KeyboardInterrupt:
+    print 'Server shutdown'
 
 
 

7786b813ba97495c47ae00c72eb4465c42eb0b23
Frederic <nouiz@nouiz.org>
2013-02-22 16:43:04 -0500

    Add script to start the process on the ip server.

---
+++ server/production.ip.sh
@@ -0,0 +1,13 @@
+#TODO once to make a virtual env in the pings project directory
+BASE_DIR=/Tmp/$USER/
+export WORKON_HOME=$BASE_DIR/.virtualenvs
+export PROJECT_HOME=$BASE_DIR/pings_proj
+mkdir -p $WORKON_HOME $PROJECT_HOME
+
+source virtualenvwrapper.sh;
+workon pings
+cd server
+
+./storage_server production.ini & sleep 5;
+./leaderboards_server production.ini & sleep 5;
+for i in 44 45 46 47 48 49 50 51; do echo $i; paster serve production.ini http_port=65$i &>> serve$i.$HOSTNAME& done

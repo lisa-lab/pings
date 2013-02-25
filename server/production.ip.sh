91c8046170e35f7ac62fc6885248cb9b3ea8732b
Frederic <nouiz@nouiz.org>
2013-02-25 11:10:45 -0500

    Run the ip server from the local disk.

--- server/production.ip.sh
+++ server/production.ip.sh
@@ -8,6 +8,8 @@ source virtualenvwrapper.sh;
 workon pings
 cd server
 
+/Tmp/bastienf/nginx/sbin/nginx & sleep 5;
+redis-server redis.conf & sleep 5;
 ./storage_server production.ini & sleep 5;
 ./leaderboards_server production.ini & sleep 5;
 for i in 44 45 46 47 48 49 50 51; do echo $i; paster serve production.ini http_port=65$i &>> serve$i.$HOSTNAME& done

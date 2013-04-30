1f2f4add213cd14d3b616e66db6c5629b3f9caee
Frederic <nouiz@nouiz.org>
2013-04-30 15:02:35 -0400

    Move the server directory.

--- server/production.ip.sh
+++ server/production.ip.sh
@@ -1,5 +1,6 @@
 #TODO once to make a virtual env in the pings project directory
 BASE_DIR=/Tmp/$USER/
+BASE_DIR=/opt/ping/
 export WORKON_HOME=$BASE_DIR/.virtualenvs
 export PROJECT_HOME=$BASE_DIR/pings_proj
 mkdir -p $WORKON_HOME $PROJECT_HOME
@@ -8,7 +9,7 @@ source virtualenvwrapper.sh;
 workon pings
 cd server
 
-/Tmp/bastienf/nginx/sbin/nginx & sleep 5;
+${BASE_DIR}/nginx/sbin/nginx & sleep 5;
 redis-server redis.conf & sleep 5;
 ./storage_server production.ini & sleep 5;
 ./leaderboards_server production.ini & sleep 5;
